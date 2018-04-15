e7c575612fc052d7c0b9266d0b16e11dbc85c8dc
Nick Zhao <nickzrz@hotmail.com>
2018-04-15 14:31:29 -0700

    Need to finish all

---
+++ Bubble_Sort/Bubble_Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static int[] RandomArray(Random rand)
+        {
+            int[] array = new int[1];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next();
+            }
+            return array;
+            //throw new NotImplementedException();
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] Array = RandomArray(new Random());
+
+            for(int i = 0; i < Array.Length; i++)
+            {
+                if(Array[0] < Array[1])
+                {
+                    Array[0]++;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
