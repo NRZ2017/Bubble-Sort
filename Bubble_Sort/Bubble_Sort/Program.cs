99c1f5c7ca4da2c12ca562dcdc2594fe1be851fd
Nick Zhao <nickzrz@hotmail.com>
2018-04-22 14:34:11 -0700

    Finished bubble, insertion, selection sort

--- Bubble_Sort/Bubble_Sort/Program.cs
+++ Bubble_Sort/Bubble_Sort/Program.cs
@@ -8,30 +8,98 @@ namespace Bubble_Sort
 {
     class Program
     {
-        static int[] RandomArray(Random rand)
+        static int[] RandomArray(int size, Random rand)
         {
-            int[] array = new int[1];
-            for(int i = 0; i < array.Length; i++)
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rand.Next();
+                array[i] = rand.Next(0, size);
             }
             return array;
-            //throw new NotImplementedException();
         }
 
 
-        static void Main(string[] args)
+        static int[] BubbleSort(int[] array)
         {
-            int[] Array = RandomArray(new Random());
-
-            for(int i = 0; i < Array.Length; i++)
+            bool sorting = true;
+            while (sorting)
             {
-                if(Array[0] < Array[1])
+                sorting = false;
+                for (int i = 1; i < array.Length; i++)
                 {
-                    Array[0]++;
+                    if (array[i] < array[i - 1])
+                    {
+                        sorting = true;
+                        //swap
+                        int tempValue = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = tempValue;
+                    }
+
                 }
             }
+            return array;
 
+        }
+
+        static int[] SelectionSort(int[] array)
+        {
+            //find the smallest number in the array
+
+            for (int j = 0; j < array.Length; j++)
+            {
+
+                int small = j;
+                for (int i = j + 1; i < array.Length; i++)
+                {
+                    if (array[i] < array[small])
+                    {
+                        small = i;
+                    }
+                }
+                //swap swap starting index with small index
+                int TempValue = array[small];
+                array[small] = array[j];
+                array[j] = TempValue;
+            }
+
+            return array;
+        }
+
+        static int[] InsertionSort(int[] array)
+        {
+
+            for (int sorted = 0; sorted < array.Length - 1; sorted++)
+            {
+                for (int i = sorted + 1; i > 0; i--)
+                {
+                    if (array[i] < array[i - 1])
+                    {
+                        int tempValue = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = tempValue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = RandomArray(10, new Random());
+
+            //array = BubbleSort(array);
+            //array = SelectionSort(array);
+            array = InsertionSort(array);
+            //print array
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
             Console.ReadKey();
         }
     }
